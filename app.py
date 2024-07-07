95dd84e6f0154e717b036fa1577efa807237e1b6
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-07-07 16:00:46 -0700

    init port for Render

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def insert_simulated_ligation():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
