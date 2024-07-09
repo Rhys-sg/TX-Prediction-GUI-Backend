c3938dcdca75d429ace62ea1c0f4bcef2a05857a
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-07-09 16:19:47 -0700

    add send_file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from keras.saving import load_model
 from dotenv import load_dotenv
