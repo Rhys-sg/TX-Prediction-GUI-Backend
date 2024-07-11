10ce348eef4b105868f6fbde706c691c40b2fae7
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-07-10 17:38:14 -0700

    add host and port

--- app.py
+++ app.py
@@ -140,4 +140,5 @@ def get_valid_domain():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 1000))
+    app.run(host='0.0.0.0', port=port)
