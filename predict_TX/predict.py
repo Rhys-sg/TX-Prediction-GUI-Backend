5dee1dd7d27b99b9328f834917cafcad67a0b7a8
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-09-08 21:11:43 -0700

    update predict to use model 5.0

--- predict_TX/predict.py
+++ predict_TX/predict.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 
 def predict(seq, model):
-    full_seq = get_full_seq(seq)
+    full_seq = get_full_seq(seq, model)
     encoded_seq = one_hot_encode(full_seq)
     prediction = model.predict(encoded_seq)[0][0]
     return float(prediction.item())
 
-def get_full_seq(seq):
-    return 'TTAATACTAGAGGTCTTCCGAC' + seq[0:6] + '0' + seq[6:] + 'GCGGGAAGACAACTAGGGGCCCA'
+def get_full_seq(seq, model):
+    insert = 'TTAATACTAGAGGTCTTCCGAC' + seq + 'GCGGGAAGACAACTAGGGGCCCA'
+    input_len = model.input_shape[1]
+    return '0' * (input_len - len(insert)) + insert
 
 def one_hot_encode(sequence):
     mapping = {'A': [1,0,0,0], 'C': [0,1,0,0], 'G': [0,0,1,0], 'T': [0,0,0,1], '0': [0,0,0,0]}
