2643b76d7850728728f0a4aa5f9f038bfa13d885
Rhys Sorenson-Graff <sorensor@whitman.edu>
2025-05-12 10:50:15 -0700

    Update linear regression

--- predict_TX/predict.py
+++ predict_TX/predict.py
@@ -3,23 +3,24 @@ import pandas as pd
 
 def predict(seq, model):
     # Calculated using linear regression between predicted values and log(relative fluorescence) values
-    slope = 19828034
-    intercept = -7102957
+    slope = 2.015845699353661
+    intercept = 5.290007180011913
 
     full_seq = get_full_seq(seq, model)
     encoded_seq = one_hot_encode(full_seq)
     prediction = model.predict(encoded_seq)[0][0]
-    return convert_prediction_to_fluorescence(float(prediction.item()), slope, intercept)
+
+    log_Rel_RFP = slope * prediction + intercept
+    Rel_RFP = 10 ** log_Rel_RFP
+
+    return max(Rel_RFP, 50001)
 
 def get_full_seq(seq, model):
-    insert = 'TTAATACTAGAGGTCTTCCGAC' + seq + 'GCGGGAAGACAACTAGGGGCCCA'
+    insert = 'AATACTAGAGGTCTTCCGAC' + seq + 'GCGGGAAGACAACTAGGGG'
     input_len = model.input_shape[1]
-    return '0' * (input_len - len(insert)) + insert
+    return insert.zfill(input_len)
 
 def one_hot_encode(sequence):
     mapping = {'A': [1,0,0,0], 'C': [0,1,0,0], 'G': [0,0,1,0], 'T': [0,0,0,1], '0': [0,0,0,0]}
     encoding = [mapping[nucleotide] for nucleotide in sequence]
     return np.array([encoding])
-
-def convert_prediction_to_fluorescence(prediction, slope, intercept):
-    return max(slope * prediction + intercept, 50001)
